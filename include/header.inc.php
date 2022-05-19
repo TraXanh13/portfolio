071bace33d6f0cb5e63c4ecb49e61ec383cbaaee
TraXanh13 <kyleduong13@gmail.com>
2022-05-19 11:45:17 -0600

    Landing page is done

---
+++ include/header.inc.php
@@ -0,0 +1,8 @@
+<header>
+    <a href="index.html"><img src="./images/logo.png" alt="Image"></a>
+    <nav>
+        <a href="projects.html">Projects</a>
+        <a href="experience.html">Experience</a>
+        <a href="contact.html">Contact</a>
+    </nav>
+</header>
