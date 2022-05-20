2f288e9f861df7cde79205452aab51688ddaee57
TraXanh13 <kyleduong13@gmail.com>
2022-05-20 00:52:09 -0600

    working on experience

--- include/header.inc.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <a href="index.html"><img src="./images/logo.png" alt="Image"></a>
-    <nav>
-        <a href="projects.html">Projects</a>
-        <a href="experience.html">Experience</a>
-        <a href="contact.html">Contact</a>
-    </nav>
-</header>
